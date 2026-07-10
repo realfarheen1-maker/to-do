8b017fa8caa9230a0e9360748df6928b8c6639b8
Farheen Khan <realfarheen6@gmail.com>
2026-07-10 00:26:20 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+var db=[]
+
+
+const http= require('http')
+const { stringify } = require('querystring')
+const server= http.createServer((req,res)=> {
+var str=""
+console.log(req.method,req.url)
+req.on("data",(data)=>{
+ str+=data
+})
+req.on("end",()=>{
+    if(req.method=="POST")
+        { db.push(JSON.parse(str))   
+        }
+        
+        res.end(JSON.stringify (db))
+})
+
+})
+server.listen(3000,()=>{
+    console.log("server listening")
+})
